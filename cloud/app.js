fd8ddefeb201ee5d3dd7d4265d127e909577ccd1
daniel51 <daniel51@126.com>
2015-04-27 17:40:14 +0800

    上传测试

--- cloud/app.js
+++ cloud/app.js
@@ -23,29 +23,29 @@ app.get('/jsonTest', function(req, res) {
 //var upload = require('cloud/upload.js');
 //app.use('/upload', upload);
 
-// 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
-//app.get('/upload', function(req, res) {
-//    res.render('upload', {
-//        title: '上传测试'
-//    });
-//});
-//
-//var fs = require('fs');
-//app.post('/upload', function(req, res){
-//    var iconFile = req.files.iconImage;
-//    if(iconFile){
-//        fs.readFile(iconFile.path, function(err, data){
-//            if(err)
-//                return res.send('读取文件失败');
-//            var base64Data = data.toString('base64');
-//            var theFile = new AV.File(iconFile.name, {base64: base64Data});
-//            theFile.save().then(function(theFile){
-//                res.send('上传成功！');
-//            });
-//        });
-//    }else
-//        res.send('请选择一个文件。');
-//});
+
+app.get('/upload', function(req, res) {
+    res.render('upload', {
+        title: '上传测试'
+    });
+});
+
+var fs = require('fs');
+app.post('/upload', function(req, res){
+    var iconFile = req.files.iconImage;
+    if(iconFile){
+        fs.readFile(iconFile.path, function(err, data){
+            if(err)
+                return res.send('读取文件失败');
+            var base64Data = data.toString('base64');
+            var theFile = new AV.File(iconFile.name, {base64: base64Data});
+            theFile.save().then(function(theFile){
+                res.send('上传成功！');
+            });
+        });
+    }else
+        res.send('请选择一个文件。');
+});
 
 
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
